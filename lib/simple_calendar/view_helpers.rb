07738b2cf3cdd186cde0f5a5742ba53f32513a35
aps <alex.pszczolkowski@gmail.com>
2013-11-18 00:42:04 +0100

    tweaks in inline calendar

--- lib/simple_calendar/view_helpers.rb
+++ lib/simple_calendar/view_helpers.rb
@@ -12,7 +12,7 @@ module SimpleCalendar
       range          = build_range selected_month, options
       month_array    = range.each_slice(7).to_a
 
-      if options[:params].has_key?(:inline) && options[:params][:inline] == true
+      if options.has_key?(:inline) && options[:inline] == true
         draw_inline_calendar(selected_month, month_array, current_date, events, options, block)
       else
         draw_calendar(selected_month, month_array, current_date, events, options, block)
@@ -24,18 +24,26 @@ module SimpleCalendar
       {
           :year           => (params[:year] || Time.zone.now.year).to_i,
           :month          => (params[:month] || Time.zone.now.month).to_i,
+          :day            => (params[:day] || Time.zone.now.day).to_i,
           :prev_text      => raw("&laquo;"),
           :next_text      => raw("&raquo;"),
           :start_day      => :sunday,
           :class          => "table table-bordered table-striped calendar",
           :params         => {},
-          :time_selector  => "start_time"
+          :time_selector  => "start_time",
+          :inline         => false,
+          :one_month_only => false
       }
     end
     # Returns array of dates between start date and end date for selected month
     def build_range(selected_month, options)
-      start_date = selected_month.beginning_of_month.beginning_of_week(options[:start_day])
-      end_date   = selected_month.end_of_month.end_of_week(options[:start_day])
+      if options.has_key?(:one_month_only) && options[:one_month_only] == true
+        start_date = selected_month.beginning_of_month
+        end_date   = selected_month.end_of_month
+      else 
+        start_date = selected_month.beginning_of_month.beginning_of_week(options[:start_day])
+        end_date   = selected_month.end_of_month.end_of_week(options[:start_day])
+      end
 
       (start_date..end_date).to_a
     end
@@ -43,12 +51,21 @@ module SimpleCalendar
     def draw_inline_calendar(selected_month, month, current_date, events, options, block)
       tags = []
       today = Time.zone.now.to_date
+      previous_month = selected_month.advance :months => -1
+      next_month = selected_month.advance :months => 1
+
+      tags << content_tag(:div, :class => "month-name") do 
+        "#{I18n.t("date.month_names")[selected_month.month]} #{selected_month.year}"
+      end
+
       tags << month_link(options[:prev_text], previous_month, options[:params], {:class => "previous-month"})
       content_tag(:div, :class => "#{options[:class]} inline-calendar") do
         day_names = I18n.t("date.abbr_day_names")
         day_names = day_names.rotate((Date::DAYS_INTO_WEEK[options[:start_day]] + 1) % 7)
-        tags << content_tag(:div, day_names.collect { |name| content_tag :span, name, :class => (selected_month.month == today.month && today.strftime("%a") == name ? "current-day" : nil)}.join.html_safe)
-        tags << content_tag(:div, :'data-month'=>selected_month.month, :'data-year'=>selected_month.year) do
+        tags << content_tag(:div, :class => "day_names") do
+          day_names.collect { |name| content_tag :span, name, :class => (selected_month.month == today.month && today.strftime("%a") == name ? "current-day" : nil) }.join.html_safe
+        end
+        tags << content_tag(:span, :class => "days_list", :'data-month'=>selected_month.month, :'data-year'=>selected_month.year) do
 
           month.collect do |week|
             content_tag(:span, :class => (week.include?(today) ? "current-week week" : "week")) do
@@ -58,6 +75,7 @@ module SimpleCalendar
                 day_class << "today" if today == date
                 day_class << "not-current-month" if selected_month.month != date.month
                 day_class << "past" if today > date
+                day_class << "selected" if date.day.to_s == params[:day]
                 day_class << "future" if today < date
                 day_class << "wday-#{date.wday.to_s}" # <- to enable different styles for weekend, etc
 
@@ -66,18 +84,16 @@ module SimpleCalendar
                 day_class << (cur_events.any? ? "events" : "no-events")
 
                 content_tag(:span, :class => day_class.join(" "), :'data-date-iso'=>date.to_s, 'data-date'=>date.to_s.gsub('-', '/')) do
-                  content_tag(:span) do
                     spans = []
-                    concat content_tag(:span, date.day.to_s, :class=>"day_number")
+                    concat content_tag(:a, date.day.to_s, :href=>"/performances/search?year=#{(params[:year].blank?) ? Time.now.year : params[:year]}&month=#{(params[:month].blank?) ? Time.new.month : params[:month] }&city=#{params[:city]}&title=#{params[:title]}&day=#{date.day.to_s}", :class=>"day_number")
 
                     if cur_events.empty? && options[:empty_date]
                       concat options[:empty_date].call(date)
                     else
-                      spans << cur_events.collect{ |event| block.call(event) }
+                      #spans << cur_events.collect{ |event| block.call(event) }
                     end
 
                     spans.join.html_safe
-                  end #content_tag :div
                 end #content_tag :td
 
               end.join.html_safe
